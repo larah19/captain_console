25286b257c0ac32c35f663e516d5654648d5fe38
eysta00_ <eysi@live.com>
2020-05-12 18:20:51 +0000

    merging

--- captain_console/urls.py
+++ captain_console/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import console
-
+# '' = Default home page
 urlpatterns = [
     path('', include('console.urls')),
     path('admin/', admin.site.urls),
